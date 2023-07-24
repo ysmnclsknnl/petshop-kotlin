c0a037354fe533be7b178a95acebe0c576761827
Yasemin Caliskan <>
2023-07-24 11:17:31 +0200

    initalization

---
+++ src/main/kotlin/com/example/petshopkotlin/PetshopKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.petshopkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetshopKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<PetshopKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/petshopkotlin/PetshopKotlinApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.petshopkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetshopKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
