a56f24c369f70ae84d813fba469c627a2f9037dd
Yasemin Caliskan <>
2023-08-02 12:50:35 +0200

    attempt to fix errors

--- src/test/kotlin/com/example/petshopkotlin/pet/SecurityTesting.kt
+++ src/test/kotlin/com/example/petshopkotlin/pet/SecurityTesting.kt
@@ -2,19 +2,23 @@ package com.example.petshopkotlin.pet
 
 import com.example.petshopkotlin.pet.model.Pet
 import com.example.petshopkotlin.pet.model.PetType
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
-import org.mockito.BDDMockito.given
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithAnonymousUser
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+@ExtendWith(SpringExtension::class)
 @WebMvcTest(PetController::class)
 class SecurityTests {
 
@@ -106,19 +110,21 @@ class SecurityTests {
     @WithMockUser(roles = ["ADMIN"])
     @Test
     fun givenAdminUser_WhenCreatePet_thenCreated() {
+        val pet = Pet(
+            name = "Tom",
+            description = "Very fast cat. Loves eating fish.",
+            age = 2,
+            type = PetType.CAT,
+            adopted = true,
+            photoLink = "www.foo.com",
+        )
+
+        given(petService.addPet(pet)).willReturn(pet)
+
         mvc.perform(
-            MockMvcRequestBuilders.post("/api/pets")
+            post("/api/pets")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(
-                    """
-                {
-                    "name": "Amsterdam",
-                    "description": "Lovely and strong dog. Really good football player!",
-                    "age": 0,
-                    "type": "DOG",
-                    "photoLink": "wwww.image.com"
-                }
-                """.trimIndent()
+                .content(ObjectMapper().writeValueAsString(pet)
                 )
         )
             .andExpect(status().isCreated)
@@ -128,7 +134,7 @@ class SecurityTests {
     @Test
     fun givenOtherRolesExceptAdmin_whenCreatePet_thenForbidden() {
         mvc.perform(
-            MockMvcRequestBuilders.post("/api/pets")
+            post("/api/pets")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(
                     """
@@ -149,7 +155,7 @@ class SecurityTests {
     @Test
     fun givenUnauthenticatedUser_whenCreatePet_thenUnauthorized() {
         mvc.perform(
-            MockMvcRequestBuilders.post("/api/pets")
+            post("/api/pets")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(
                     """
