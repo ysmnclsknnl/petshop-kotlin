9012fa53f8026dcb14fef0aed1cd70ebcf643aa6
Yasemin Caliskan <>
2023-08-03 15:29:05 +0200

    refactor: remove deprecated methods

--- src/main/kotlin/com/example/petshopkotlin/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/petshopkotlin/security/SecurityConfig.kt
@@ -3,6 +3,7 @@ package com.example.petshopkotlin.security
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
@@ -12,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 
 @Configuration
 @EnableWebSecurity
@@ -36,20 +38,20 @@ class SecurityConfig(val userDetailsService: UserDetailsService) {
 
     @Bean
     @Throws(Exception::class)
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .csrf()
-            .disable()
-            .authorizeRequests()
-            .and()
-            .httpBasic()
-            .and()
-            .authorizeRequests()
-            .anyRequest()
-            .permitAll()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        return http.build()
+    fun filterChain(http: HttpSecurity): SecurityFilterChain  = http
+            .csrf {
+                it.disable()
+            }
+         .exceptionHandling {
+            it.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        }
+            .authorizeHttpRequests {
+                it.requestMatchers("get", "/api/pets").hasAnyRole("ADMIN", "CUSTOMER")
+                it.requestMatchers("post", "/api/pets").hasRole("ADMIN")
+                it.requestMatchers("patch", "/api/pets/{id}").hasRole("CUSTOMER")
+                it.requestMatchers("/api/users/login").permitAll()
+            }
+            .sessionManagement{
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }.build()
     }
-}
