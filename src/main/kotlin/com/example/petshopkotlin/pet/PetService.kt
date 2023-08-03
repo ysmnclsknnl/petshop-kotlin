869db92da606c1c7b73f45715636646bf54acd3b
Yasemin Caliskan <>
2023-08-03 15:37:38 +0200

    refactor: change the return type of validate pet

--- src/main/kotlin/com/example/petshopkotlin/pet/PetService.kt
+++ src/main/kotlin/com/example/petshopkotlin/pet/PetService.kt
@@ -10,8 +10,8 @@ class PetService(val repo: PetRepository) {
     fun getPets() = repo.findAllByOrderByIdDesc()
 
     fun addPet(pet: Pet): Pet {
-        val errors: List<String> = validatePet(pet)
-        require(errors.isEmpty()) { errors.joinToString(" ") }
+        val errors = validatePet(pet)
+        require(errors.isBlank()) { errors }
 
         return repo.save(pet)
     }
@@ -21,7 +21,6 @@ class PetService(val repo: PetRepository) {
         val pet = repo.findById(id).orElseThrow { IllegalArgumentException("Pet with ID: $id doesn't exist") }
 
         require(!pet.adopted){ "Pet with ID: $id is already adopted" }
-//        require(repo.existsAllByIdAndAdoptedFalse(id)){ "Pet with ID: $id is already adopted or don't exist" }
 
         pet.adopted = true
 
@@ -30,11 +29,10 @@ class PetService(val repo: PetRepository) {
         return "You successfully adopted ${pet.name}!"
     }
 
-    private fun validatePet(pet: Pet) = listOfNotNull(
+    internal fun validatePet(pet: Pet) = listOfNotNull(
         if (pet.name.length >= 3) null else "Name must be at least 3 characters.",
         if (pet.description.length >= 15) null else "Description must be at least 15 characters.",
         if (pet.age >= 0) null else "Age must be at least 0.",
-        if (pet.type == PetType.CAT || pet.type == PetType.DOG) null else "Pet must be DOG or CAT",
-        if (pet.photoLink.isNotBlank()) null else "Pet must have an image of 100kb ",
-    )
+        if (pet.photoLink.isNotBlank()) null else "Pet must have an image link.",
+    ).joinToString(" ")
 }
