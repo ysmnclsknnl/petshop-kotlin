53ea698201f33c5b05a45501a620aa8898857f9b
Yasemin Caliskan <>
2023-08-09 13:11:16 +0200

    authorize: getpets route

--- src/main/kotlin/com/example/petshopkotlin/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/petshopkotlin/security/SecurityConfig.kt
@@ -65,7 +65,7 @@ class SecurityConfig(val userDetailsService: UserDetailsService) {
             it.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
         }
         .authorizeHttpRequests {
-            it.requestMatchers("get", "/api/pets").permitAll()
+            it.requestMatchers("/api/pets").hasAnyRole("ADMIN", "CUSTOMER")
             it.requestMatchers("post", "/api/pets").hasRole("ADMIN")
             it.requestMatchers("patch", "/api/pets/{id}").hasRole("CUSTOMER")
             it.requestMatchers("/api/auth/**").permitAll()
